9c0774b38100ae5ca19c6ec5ec887e01670f8823
briankmbugua <briankmbuguak@gmail.com>
2023-07-29 12:24:33 +0300

    all commits

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost:3306/one_to_many_relationship'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost:3306/many_to_many_relationship'
 
 db = SQLAlchemy(app)
 
